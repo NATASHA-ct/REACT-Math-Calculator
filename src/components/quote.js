b905fd8f48533d260119f07a868e27e9090de31f
NATASHA-ct <chirombenatashatatenda@gmail.com>
2022-08-16 23:25:47 +0300

    Add UI and styling

---
+++ src/components/quote.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// eslint-disable-next-line react/prefer-stateless-function
+const Quote = () => (
+  <div className="quoteCont">
+    <h3 className="quoteHead">
+      {' '}
+      {/* eslint-disable max-len */}
+      Without mathematics, there’s nothing you can do. Everything around you is
+      mathematics. Everything around you is numbers. max-len
+      <br />
+      (Shakuntala Devi), Indian writer and mental calculator, 10 Jun 1944
+    </h3>
+  </div>
+);
+
+export default Quote;
