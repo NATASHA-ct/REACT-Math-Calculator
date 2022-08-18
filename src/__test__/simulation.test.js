52a7ee130ebdae778dde901c4a9c9723806b71c5
NATASHA-ct <chirombenatashatatenda@gmail.com>
2022-08-19 01:32:55 +0300

    Add  user interaction testing

--- src/__test__/simulation.test.js
+++ src/__test__/simulation.test.js
@@ -15,3 +15,18 @@ test('When a Key is pressed the display updates automatically', async () => {
 
   expect((await display).textContent).toBe('1');
 });
+
+test('When button is clicked the display updates automatically', async () => {
+  render(
+    <MemoryRouter>
+      <Calculator />
+    </MemoryRouter>,
+  );
+
+  const calcKey = screen.getByRole('button', { name: /2/ });
+  const display = screen.findByTestId('display');
+  fireEvent.click(calcKey);
+
+  expect((await display).textContent).toBe('2');
+  // expect(screen.getByRole("alert"));
+});
